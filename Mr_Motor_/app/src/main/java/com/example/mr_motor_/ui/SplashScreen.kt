9cc4a0a67d105c91a755c2e6c26ae7b98032f424
Даниил Ефимов <daefimov_1@edu.hse.ru>
2022-03-28 18:08:08 +0300

    splash gif done

--- Mr_Motor_/app/src/main/java/com/example/mr_motor_/ui/SplashScreen.kt
+++ Mr_Motor_/app/src/main/java/com/example/mr_motor_/ui/SplashScreen.kt
@@ -1,19 +1,21 @@
 package com.example.mr_motor_.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.mr_motor_.R
-import org.apache.commons.io.IOUtils
 import pl.droidsonroids.gif.GifImageView
-import java.io.IOException
-import java.io.InputStream
+
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        setContentView(R.layout.splash_screen)
 
-        val gifImage = findViewById<GifImageView>(R.id.gif_image)
-        gifImage.setImageResource(R.drawable.loader)
+        supportActionBar?.hide()
+
+        val gifImage: GifImageView = findViewById<GifImageView>(R.id.gif_image)
+        gifImage.setImageResource(R.drawable.splashgif)
     }
 }
